f38ce180755cfc4be649c94823235fbe9def594f
PeiShan Chen <shanpsc@gmail.com>
2019-07-13 03:05:11 +0800

    DAY1 HW

--- iTallyBook/iTallyBook/Controllers/HomeController.cs
+++ iTallyBook/iTallyBook/Controllers/HomeController.cs
@@ -3,16 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using iTallyBook.Models.ViewModels;
 
 namespace iTallyBook.Controllers
 {
     public class HomeController : Controller
     {
+        /// <summary>
+        /// 首頁
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Index()
         {
-            return View();
+            List<IndexViewModel> dataList = GetAccountingDataList();
+            return View(dataList);
         }
 
+        /// <summary>
+        /// 關於
+        /// </summary>
+        /// <returns></returns>
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -20,11 +30,35 @@ namespace iTallyBook.Controllers
             return View();
         }
 
+        /// <summary>
+        /// 連絡方式
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        /// <summary>
+        /// 取得收支紀錄清單
+        /// </summary>
+        /// <returns></returns>
+        private List<IndexViewModel> GetAccountingDataList()
+        {
+            var dataList = new List<IndexViewModel>();
+            Random ran = new Random(Guid.NewGuid().GetHashCode());
+            for (int i = 0; i < 100; i++)
+            {
+                IndexViewModel item = new IndexViewModel();
+                item.Amount = ran.Next(10, 20000);
+                item.Type = item.Amount % 2 == 0 ? "支出" : "收入";
+                item.DataDate = DateTime.Now.AddDays(ran.Next(-20, 1));
+                dataList.Add(item);
+            }
+
+            return dataList.OrderByDescending(x => x.DataDate).ToList();
         }
     }
 }
---
+++ iTallyBook/iTallyBook/Models/ViewModels/IndexViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Web;
+
+namespace iTallyBook.Models.ViewModels
+{
+    public class IndexViewModel
+    {
+
+        [DisplayName("類型")]
+        public string Type { get; set; }
+
+        [DisplayName("日期")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
+        public DateTime DataDate { get; set; }
+
+        [DisplayName("金額")]
+        [DisplayFormat(DataFormatString = "{0:#,##0.##}")]
+        public decimal Amount { get; set; }
+    }
+}
